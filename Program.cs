f4be253ff2310ef3fd68d4beb4eb93d0272cba90
Rudy Gabuco Jr <140367514+rgabuco@users.noreply.github.com>
2024-06-20 06:15:35 -0600

    Added Connect4Game Class with a method to initialize players
    We'll need another GameBoard class to define the board and keep track of the game state. We can start with all human players for now.

--- Program.cs
+++ Program.cs
@@ -6,8 +6,8 @@ namespace Connect4
     {
         public string Name { get; set; }
         public char Disc { get; set; }
-
-        public Player(string name, char disc)
+        
+        public Player( string name, char disc)
         {
             Name = name;
             Disc = disc;
@@ -15,9 +15,10 @@ namespace Connect4
 
         public abstract int PlayerMove();
     }
+
     public class HumanPlayer : Player
     {
-        public HumanPlayer(string name, char disc) : base(name, disc) { }
+        public HumanPlayer(string name, char disc) : base( name, disc) { }
 
         public override int PlayerMove()
         {
@@ -27,21 +28,52 @@ namespace Connect4
             {
                 Console.WriteLine("Please enter a number between 1 and 7: ");
                 dropChoice = Convert.ToInt32(Console.ReadLine());
-
+            
             } while (dropChoice < 1 || dropChoice > 7);
 
             return dropChoice;
         }
     }
+    public class Connect4Game
+    {
+        private Player playerOne;
+        private Player playerTwo;
+        //TODO: We can define another GameBoard class to keep track of game state
 
-    //Todo: Add Computer Player class
+        public Connect4Game()
+        {
+            //TODO: We can initialize the GameBoard here
+        }
 
+        public void InitializePlayers()
+        {
+            //All human players for now
+            Console.WriteLine("Let's Play Connect 4!");
+            Console.WriteLine("Player One please enter your name: ");
+            playerOne = new HumanPlayer(Console.ReadLine(), 'X');
+            Console.WriteLine("Player Two please enter your name: ");
+            playerTwo = new HumanPlayer(Console.ReadLine(), 'O');
+        }
+
+        public void PlayGame()
+        {
+            //TODO: Define game logic
+        }
+
+        private bool RestartGame()
+        {
+            //TODO: Define logic for restarting game
+
+            return false;
+        }
+    }
 
     class Program
     {
         static void Main(string[] args)
         {
-
+            Connect4Game game = new Connect4Game();
+            game.InitializePlayers();
         }
     }
 }
