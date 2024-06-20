35026f87b4500ee8b5d13454f46c11b7b10fc3ac
Rudy Gabuco Jr <140367514+rgabuco@users.noreply.github.com>
2024-06-19 18:32:55 -0600

    Added Abstract Player Class

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Connect4
+{
+    public abstract class Player
+    {
+        public string Name { get; set; }
+        public char Disc { get; set; }
+        
+        public Player( string name, char disc)
+        {
+            Name = name;
+            Disc = disc;
+        }
+
+        public abstract int PlayerMove();
+    }
+    //Todo: Add Human Player class
+
+    //Todo: Add Computer Player class
+    
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
